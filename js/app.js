8d32675f1cb135a55643477e17c997e3a961eeba
OnlyDropped <asheburenkov@yandex.ru>
2022-05-09 01:51:38 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('.example-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-prev.jpg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-next.svg" alt=""></button>',
+});
