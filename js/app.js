39582e9d526df1f6aa8b206d1ecf1967dcce96dc
OnlyDropped <asheburenkov@yandex.ru>
2022-05-10 18:32:33 +0300

    added new changes

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ $('.example-slider').slick({
   prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-prev.jpg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-next.svg" alt=""></button>',
 });
+$('.slider').slick({
+  arrows: false,
+  autoplay: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplaySpeed: 2000
+});
