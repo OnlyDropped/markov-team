7a6a779336af8cef488f0971a47764a4859b75b4
OnlyDropped <asheburenkov@yandex.ru>
2022-05-23 22:51:12 +0300

    added new changes

--- js/app.js
+++ js/app.js
@@ -58,12 +58,12 @@ $('.professional-slider').slick({
   cssEase: 'linear',
 })
 
-$('.example-wrap').slick({
-  slidesToShow: 2,
+$('.random__inner').slick({
+  slidesToShow: 3,
   slidesToScroll: 1,
-  nextArrow: '<button type="button" class="slick-next"><img src="img/slider-tovar/next.png" alt=""></button>',
-  prevArrow: '<button type="button" class="slick-prev"><img src="img/slider-tovar/prev.png" alt=""></button>',
+  arrows: true,
+  nextArrow: '<button type="button" class="slick-next"><img src="img/icons/next-arrow.png" alt=""></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/prev-arrow.png" alt=""></button>',
   cssEase: 'linear',
-})
-
-/*осталось доработать слайдер в service.html*/
+});
+/*осталось доработать слайдер в service.html*/next-arrow
